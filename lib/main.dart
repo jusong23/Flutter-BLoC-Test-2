e1571920f8f49b64b1fe2554972a819412703fde
jusong <jusong23@naver.com>
2023-08-03 18:05:00 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:bloc_pattern/src/ui/bloc_display_wideget.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: BlocDisplayWidget() // (1) BlocDisplayWidget 호출
+        // 가장 먼저 보이는 부분
+        );
+  }
+}
